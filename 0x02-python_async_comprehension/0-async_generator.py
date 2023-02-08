1e9d242855d8cbb764acdb77748c075514a2ee41
Nosa <nosakhareaghedo42@gmail.com>
2023-02-08 00:36:47 +0000

    trying to pass checker

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ module for task number zero
 """
 import asyncio
 from random import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """
     coroutine will loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10
